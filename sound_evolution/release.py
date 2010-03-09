1a36b443266352ab08bb250fb472ce8cff07415b
Mirko Dietrich <mirko.dietrich@bccn-berlin.de>
2010-03-09 22:48:16 +0100

    updated release description

--- sound_evolution/release.py
+++ sound_evolution/release.py
@@ -1,7 +1,7 @@
 """Release information for sound-evolution"""
 
 version = "0.01"
-description = ("A recursive file conversion utility, aimed at "
-               "making it easier to get OSS audio on to iPods")
+description = ("Sound Evolution is a framework for evolving computer "
+               "sounds. It's based on the idea of genetic algorithms.")
 author = "Duncan Blythe, Mirko Dietrich, Stephan Gabler, Matthias Gloel, Rafael Schultze-Kraft"
 download_url = "http://github.com/buzz/sound-evolution"

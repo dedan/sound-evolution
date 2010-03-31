66cd2eaea6aeead374ba465f3933dade9fea8725
Stephan Gabler <stephan.gabler@gmail.com>
2010-03-31 12:18:19 +0200

    fixed again the csd adapter test

--- tests/csound_adapter_test.py
+++ tests/csound_adapter_test.py
@@ -5,33 +5,35 @@ import sound_evolution as se
 
 def setup():
     global simple_json, invalid_json, complex_json, complex_orc
+    global csd
     complex_json = open(
         os.path.join(os.path.dirname(__file__),
                      "fixtures", "complex_instrument.json")).read()
-
-def test_sound():
-    """Should play/render a given/random instrument."""
-    random = 1 # 0=no, 1=yes
-    render = 0 # 0=no, 1=yes
-
-    csd_file = "__test_out.csd"
-    aif_file = "__test_out.aif"
-            
-    if random:
-        params = {"const_prob": 0.7, "max_children": 4}
-        i = se.instrument.Instrument.random(params)
-    else:
-        global complex_json
-        i = se.instrument.Instrument(complex_json)        
-
-    csd = se.csound_adapter.CSD(csd_file, render)
-    csd.orchestra(i)
+    csd = se.csound_adapter.CSD()
+    csd.orchestra(se.instrument.Instrument(complex_json))
     csd.score('i 1 0 2')
-    csd.output(csd_file)
+
+def test_play_simple():
+    """Should play a simple instrument to sound card."""
+    global csd
+    csd.play()
+    assert os.path.exists(csd.output_csd_filename)
+    assert os.path.getsize(csd.output_csd_filename) > 0
     
-    assert(os.path.exists(csd_file))
-    assert(os.path.getsize(csd_file) > 0)
-    if render:
-        assert(os.path.exists(aif_file))
-        assert(os.path.getsize(aif_file) > 0)
+def test_play_random():
+    """Should play a random instrument to sound card."""
+    csd_r = se.csound_adapter.CSD()
+    csd_r.orchestra(se.instrument.Instrument.random(const_prob=0.7, max_children=4))
+    csd_r.score('i 1 0 2')
+    csd_r.play()
+    assert os.path.exists(csd.output_csd_filename)
+    assert os.path.getsize(csd.output_csd_filename) > 0
     
+    
+def test_render():
+    """Should render a given instrument."""
+    global csd
+    aif_file = "/tmp/__test_out.aif"
+    csd.output_aif(aif_file)
+    assert os.path.exists(aif_file)
+    assert os.path.getsize(aif_file) > 0
